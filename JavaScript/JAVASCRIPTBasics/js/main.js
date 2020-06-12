d0d09d3959e444728f38f5d9e526f6835d891bb2
Shouko0522 <iamshk0522@gmail.com>
2020-06-12 22:26:57 +0900

    created Javascriptbasic folder

---
+++ JavaScript/JAVASCRIPTBasics/js/main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const score = 5;
+
+// if (scpre >= 80) {
+//   console.log('greate')
+// } else {
+//   console.log ('ok')
+// }
+
+// scpre >= 80 ? console.log('greate') : console.log ('ok')
+
+// const score = 60;
+// const name = 'taguchi';
+
+// if (score >= 50){
+//   if (name === 'taguchi'){
+//   console.log('Good job');
+// }
+// }
+
+// if (score >= 50 && name === 'taguchi'){
+//   console.log('Good job');
+// }
+
+// switch (signal) {
+//   case 'red':
+//   concole.log('stop');
+//   break;
+//   case 'blue':
+//   console.log('go');
+//   break;
+// }
+
+// for(let i = 1; i <= 10; i++){
+//   console.log('hello'+ ${i});
+// }
+
+// let hp =100;
+
+// while (hp > 0){
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+//
+
+// function showAdd(message){
+//   console.log(`::::::${message}::::::::``)
+// }
+
+// showAdd('Hi');
+
+function sum(a, b, c){
+  return a + b + c;
+}
+
+const sum = function(a, b, c){
+  return a + b + c;
+};
+
+const sum = (a, b, c) => a + b + c;
+
+
+
+sum(1, 2 ,3)
