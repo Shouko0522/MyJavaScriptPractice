cd1a780f440258cd1770569cb62dd8fa62cb8620
Shouko0522 <iamshk0522@gmail.com>
2020-07-01 21:50:25 +0900

    created mikuji

--- JavaScript/MYOMIKUJI/js/main.js
+++ JavaScript/MYOMIKUJI/js/main.js
@@ -4,6 +4,20 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = [1, 2, 3];
+    const n = Math.floor(Math.random() * results.length);
+        btn.textContent = results[n];
+
+        // switch(n){
+        //   case 0:
+        //   btn.textContent = '大吉';
+        //   break;
+        //   case 1:
+        //   btn.textContent = '中吉';
+        //   break;
+        //   case 2:
+        //   btn.textContent = '木r';
+        //   break;
+        // }
   })
 }
