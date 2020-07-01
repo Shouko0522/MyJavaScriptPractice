0c536c86952f42446f0b8ad7af3c0c2e7a3aac0d
Shouko0522 <iamshk0522@gmail.com>
2020-07-01 21:55:39 +0900

    incresed probability

--- JavaScript/MYOMIKUJI/js/main.js
+++ JavaScript/MYOMIKUJI/js/main.js
@@ -4,20 +4,18 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = [1, 2, 3];
-    const n = Math.floor(Math.random() * results.length);
-        btn.textContent = results[n];
+    // const results = [1, 2, 3];
+    // const n = Math.floor(Math.random() * results.length);
+    //     btn.textContent = results[n];
+    const n = Math.random();
+    if (n < 0.05){
+      btn.textContent = "Unlucky";
 
-        // switch(n){
-        //   case 0:
-        //   btn.textContent = '大吉';
-        //   break;
-        //   case 1:
-        //   btn.textContent = '中吉';
-        //   break;
-        //   case 2:
-        //   btn.textContent = '木r';
-        //   break;
-        // }
+    } else {
+      btn.textContent = "Lucky";
+
+    }
+
+
   })
 }
